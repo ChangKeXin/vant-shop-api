336f1bf7dec08472519c144e34e9df4f32204651
fortheday <fortheday@126.com>
2019-11-08 09:50:18 +0800

    添加商品列表接口

--- index.js
+++ index.js
@@ -63,6 +63,47 @@ app.get('/api/v1/index_categories', (req, res) => {
     })
 })
 
+// 商品接口
+app.get('/api/v1/index_goods', (req, res)=>{
+
+    // 接收 page 和 per_page 两个参数
+    // 如果没有传默认是1 和 20
+    let page     = req.query.page || 1             // 接收 page 参数，如果为空就得到 1
+    let per_page = req.query.per_page || 20          // 接收 per_page 参数，如果为空，默认20
+
+    // 取出某一页的 20 条
+    // 翻页的原理：SQL 加 LIMIT 
+    // LIMIT x,y      x：开始的下标    y：取的数量
+    // 第 1 页的 20 条  ： LIMIT 0,20
+    // 第 2 页的 20 条  ： LIMIT 20,20  （从第21条记录开始取，取20条）
+    // 第 3 页的 20 条  ： LIMIT 40,20  （从第41条记录开始取，取20条）
+    // 第 n 页的 20 条  :  LIMIT (n-1)*20    ,   20
+
+    // 开始的下标 （当前页-1）*每页条数
+    let offset = (page-1)*per_page
+    let sql = `SELECT * FROM shop_goods LIMIT ${offset},${per_page}`
+
+    // 执行 SQL 语句
+    // err：执行SQL 的错误信息
+    // data：返回的fjs
+    db.query(sql, (err, data)=>{
+        
+        // 如果失败就返回错误信息，如果成功就返回数据
+        if(err) {
+            // 返回前数据
+            res.json({
+                'ok': 0,
+                'error': err
+            })
+        } else {
+            res.json({
+                'ok': 1,
+                'data': data
+            })
+        }
+    })
+})
+
 // 启动服务器
 app.listen(9999, ()=>{
     console.log('成功！监听：127.0.0.1:9999!')
